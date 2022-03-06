ed9f8c4f83f3feccf7277bf8b83a3c1b44414e23
felixmare <felixen97@gmail.com>
2022-03-06 23:08:17 +0100

    test

--- pokergame.py
+++ pokergame.py
@@ -3,8 +3,15 @@ from pokermodel import *
 from card_view_ny import *
 import sys
 
-game = GameModel()
-window = SetupWindow(game)
-window.show()
+def main():
 
-app.exec_()
+
+    app = QApplication(sys.argv)
+    game = GameModel()
+    window = SetupWindow(game)
+    window.show()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
+
